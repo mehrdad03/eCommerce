d303f5252ea882e94e1741ef822ff518e23c6b40
rasoul <rasoulansarinavan98@gmail.com>
2023-02-07 18:04:51 +0330

    add the views

---
+++ app/Http/Livewire/Client/Auth/Register.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Client\Auth;
+
+use Livewire\Component;
+
+class Register extends Component
+{
+    public function render()
+    {
+        return view('livewire.client.auth.register');
+    }
+}
