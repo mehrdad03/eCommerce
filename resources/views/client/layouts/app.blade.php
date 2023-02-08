4f0beb8b82b79dd61dab6bfc912b2dc33fa802e0
rasoul <rasoulansarinavan98@gmail.com>
2023-02-08 21:39:53 +0330

    add the assets folder and updated footer of the client

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -17,6 +17,87 @@
 <body>
 {{--<livewire:header/>--}}
 
+<div class="sidebar-left"><a class="btn btn-open" href="#"></a>
+    <ul class="menu-icons hidden">
+        <li><a href="#"><img src="assets/imgs/template/monitor.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/mobile.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/game.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/clock.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/airpod.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/airpods.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/mouse.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/music-play.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/bluetooth.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/clound.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/electricity.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/cpu.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/devices.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/driver.svg" alt="Ecom"></a></li>
+        <li><a href="#"><img src="assets/imgs/template/lamp.svg" alt="Ecom"></a></li>
+    </ul>
+    <ul class="menu-texts menu-close">
+        <li class="has-children"><a href="shop-grid-2.html"><span class="img-link"><img src="assets/imgs/template/monitor.svg" alt="Ecom"></span><span class="text-link">Computers &amp; Accessories</span></a>
+            <ul class="sub-menu">
+                <li><a href="shop-grid.html">Computer Accessories</a></li>
+                <li><a href="shop-grid.html">Computer Cases</a></li>
+                <li><a href="shop-grid.html">Laptop</a></li>
+                <li><a href="shop-grid.html">HDD</a></li>
+                <li><a href="shop-grid.html">RAM</a></li>
+                <li><a href="shop-grid.html">Headphone</a></li>
+            </ul>
+        </li>
+        <li class="has-children"><a href="#"><span class="img-link"><img src="assets/imgs/template/mobile.svg" alt="Ecom"></span><span class="text-link">Cell Phones</span></a>
+            <ul class="sub-menu">
+                <li><a href="shop-grid.html">Phone Accessories</a></li>
+                <li><a href="shop-grid.html">Phone Cases</a></li>
+                <li><a href="shop-grid.html">Postpaid Phones</a></li>
+                <li><a href="shop-grid.html">Unlocked Phones</a></li>
+                <li><a href="shop-grid.html">Prepaid Phones</a></li>
+                <li><a href="shop-grid.html">Prepaid Plans</a></li>
+                <li><a href="shop-grid.html">Refurbished Phones</a></li>
+                <li><a href="shop-grid.html">Straight Talk</a></li>
+                <li><a href="shop-grid.html">iPhone</a></li>
+                <li><a href="shop-grid.html">Samsung Galaxy</a></li>
+                <li><a href="shop-grid.html">Samsung Galaxy</a></li>
+                <li><a href="shop-grid.html">Samsung Galaxy</a></li>
+                <li><a href="shop-grid.html">Samsung Galaxy</a></li>
+                <li><a href="shop-grid.html">Samsung Galaxy</a></li>
+            </ul>
+        </li>
+        <li class="has-children"><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/game.svg" alt="Ecom"></span><span class="text-link">Gaming Gatgets</span></a>
+            <ul class="sub-menu">
+                <li><a href="shop-grid.html">Wireless Routers</a></li>
+                <li><a href="shop-grid.html">Cool New Gadgets</a></li>
+                <li><a href="shop-grid.html">Tech and Gadgets</a></li>
+                <li><a href="shop-grid.html">Geek Gifts and Gadgets</a></li>
+                <li><a href="shop-grid.html">Xbox Accessories</a></li>
+                <li><a href="shop-grid.html">PlayStation Accessories</a></li>
+            </ul>
+        </li>
+        <li class="has-children"><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/clock.svg" alt="Ecom"></span><span class="text-link">Smart watches</span></a>
+            <ul class="sub-menu">
+                <li><a href="shop-grid.html">Smart Watches</a></li>
+                <li><a href="shop-grid.html">Fashion Smart Watches</a></li>
+                <li><a href="shop-grid.html">Smart Bracelets</a></li>
+                <li><a href="shop-grid.html">Pocket Watches</a></li>
+                <li><a href="shop-grid.html">Smart Rings</a></li>
+                <li><a href="shop-grid.html">Other Watches</a></li>
+            </ul>
+        </li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/airpods.svg" alt="Ecom"></span><span class="text-link">Wired Headphone</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/mouse.svg" alt="Ecom"></span><span class="text-link">Mouse &amp; Keyboard</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/music-play.svg" alt="Ecom"></span><span class="text-link">Headphone</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/bluetooth.svg" alt="Ecom"></span><span class="text-link">Bluetooth devices</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/clound.svg" alt="Ecom"></span><span class="text-link">Cloud Software</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/electricity.svg" alt="Ecom"></span><span class="text-link">Electric accessories</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/cpu.svg" alt="Ecom"></span><span class="text-link">Mainboard &amp; CPU</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/devices.svg" alt="Ecom"></span><span class="text-link">Desktop PC</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/driver.svg" alt="Ecom"></span><span class="text-link">Speaker</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/airpod.svg" alt="Ecom"></span><span class="text-link">Bluetooth Phone</span></a></li>
+        <li><a href="shop-grid.html"><span class="img-link"><img src="assets/imgs/template/lamp.svg" alt="Ecom"></span><span class="text-link">Computer Decor</span></a></li>
+    </ul>
+</div>
+
 {{--{{$slot}}--}}
 
 {{--<livewire:footer/>--}}
