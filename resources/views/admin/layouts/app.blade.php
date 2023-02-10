c8d77308c69ac347f18d1dc3a845f28b5d1e6930
rasoul <rasoulansarinavan98@gmail.com>
2023-02-11 02:30:44 +0330

    add admin views file

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/theme/favicon.svg">
+    <link href="assets/css/style.css?v=1.0.0" rel="stylesheet">
+    <title>Ecom - Marketplace Dashboard Template</title>
+</head>
+<body>
+<div class="screen-overlay"></div>
+
+
+<script src="assets/js/vendors/jquery-3.6.0.min.js"></script>
+<script src="assets/js/vendors/bootstrap.bundle.min.js"></script>
+<script src="assets/js/vendors/select2.min.js"></script>
+<script src="assets/js/vendors/perfect-scrollbar.js"></script>
+<script src="assets/js/vendors/jquery.fullscreen.min.js"></script>
+<script src="assets/js/vendors/chart.js"></script>
+<script src="assets/js/main.js?v=1.0.0"></script>
+<script src="assets/js/custom-chart.js" type="text/javascript"></script>
+</body>
+</html>
--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,3 +1,11 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-</div>
+<footer class="main-footer font-xs">
+    <div class="row pb-30 pt-15">
+        <div class="col-sm-6">
+            <script>document.write(new Date().getFullYear())</script> &copy;, Ecom - HTML Ecommerce Template .
+        </div>
+        <div class="col-sm-6">
+            <div class="text-sm-end">All rights reserved</div>
+        </div>
+    </div>
+</footer>
+
