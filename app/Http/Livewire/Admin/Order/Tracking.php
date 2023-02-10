61482fb6d42e5b3c983d1185a97ce7d13c29ce9b
rasoul <rasoulansarinavan98@gmail.com>
2023-02-11 02:43:26 +0330

    add admin assets file

---
+++ app/Http/Livewire/Admin/Order/Tracking.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Order;
+
+use Livewire\Component;
+
+class Tracking extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.order.tracking');
+    }
+}
---
+++ app/Http/Livewire/Admin/Product/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Product;
+
+use Livewire\Component;
+
+class Category extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.product.category');
+    }
+}
