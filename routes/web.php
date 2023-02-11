1434261d44342e4516421683f66afba7bab45d9e
rasoul <rasoulansarinavan98@gmail.com>
2023-02-11 14:37:01 +0330

    add routs file

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+
+    Route::get('/index', Index::class)->name('index');
+
+    Route::prefix('colors')->name('colors.')->group(function () {
+
+        Route::get('/index', App\Http\Livewire\Admin\colors\Index::class)->name('index');
+        Route::get('/create', App\Http\Livewire\Admin\colors\Create::class)->name('create');
+        Route::get('/edit/{id}', App\Http\Livewire\Admin\colors\Edit::class)->name('edit');
+    });
+    Route::prefix('sizes')->name('sizes.')->group(function () {
+
+        Route::get('/index', App\Http\Livewire\Admin\sizes\Index::class)->name('index');
+        Route::get('/create', App\Http\Livewire\Admin\sizes\Create::class)->name('create');
+        Route::get('/edit/{id}', App\Http\Livewire\Admin\sizes\Edit::class)->name('edit');
+    });
+    Route::prefix('brands')->name('brands.')->group(function () {
+
+        Route::get('/index', App\Http\Livewire\Admin\brands\Index::class)->name('index');
+        Route::get('/create', App\Http\Livewire\Admin\brands\Create::class)->name('create');
+        Route::get('/edit/{id}', App\Http\Livewire\Admin\brands\Edit::class)->name('edit');
+    });
+
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function () {
+        Route::get('/login', function () {
+            return view('admin.login');
+        });
+        Route::get('/login', Login::class)->name('login');
+    });
+    Route::middleware(['guest:admin', 'PreventBackHistory'])->group(function () {
+        Route::get('/register', function () {
+            return view('admin.register');
+        });
+        Route::get('/register', Register::class)->name('register');
+    });
+//    });
+});
