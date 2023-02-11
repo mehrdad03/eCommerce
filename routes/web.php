4442c231aecf64058940f96987964394fafb14be
LeGion <mehrdad.dadashi@gmail.com>
2023-02-11 13:31:20 +0300

    write routes structure for admin panel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*********** ADMIN PANEL *************/
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+
+
+
+});
+
+/*********** ADMIN PANEL *************/
