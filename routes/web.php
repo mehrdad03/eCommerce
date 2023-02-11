d94fe7ece38f03a7529f8cca4ba6075e2ce5dd0f
rasoul <rasoulansarinavan98@gmail.com>
2023-02-11 14:28:27 +0330

    add routs

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,21 @@ Route::prefix('admin')->name('admin.')->group(function () {
             return view('admin.home');
         })->name('home');
 
-        Route::prefix('brands')->name('brands.')->group(function () {
+        Route::prefix('brand')->name('brands.')->group(function () {
 
             Route::get('/index', App\Http\Livewire\Admin\index\livewire\brand\index::class)->name('index');
             Route::get('/create', App\Http\Livewire\Admin\index\livewire\brand\create::class)->name('create');
             Route::get('/edit', App\Http\Livewire\Admin\index\livewire\brand\edit::class)->name('edit');
         });
 
-        Route::prefix('sizes')->name('categories.')->group(function () {
+        Route::prefix('size')->name('categories.')->group(function () {
 
             Route::get('/index', App\Http\Livewire\Admin\index\livewire\size\index::class)->name('index');
             Route::get('/create', App\Http\Livewire\Admin\index\livewire\size\create::class)->name('create');
             Route::get('/edit', App\Http\Livewire\Admin\index\livewire\size\edit::class)->name('edit');
         });
 
-        Route::prefix('colors')->name('products.')->group(function () {
+        Route::prefix('color')->name('products.')->group(function () {
 
             Route::get('/index', App\Http\Livewire\Admin\index\livewire\color\index::class)->name('index');
             Route::get('/create', App\Http\Livewire\Admin\index\livewire\color\create::class)->name('create');
