94ca10c43b97b279a179742efef17967073b806c
rasoul <rasoulansarinavan98@gmail.com>
2023-02-12 20:54:30 +0330

    add dynamic color view

--- app/Http/Livewire/Admin/Color/Index.php
+++ app/Http/Livewire/Admin/Color/Index.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Livewire\Admin\Color;
 
+use App\Models\Color;
 use Livewire\Component;
 
 class Index extends Component
 {
+
+    public $name = "";
+    public $code = "";
+
+    public $colors;
+
+    public function save()
+    {
+        $this->validate([
+            'name' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u',
+            'code' => 'required | regex:/^[ا-یa-zA-Z0-9@$#^%&*!]+$/u'
+        ]);
+
+        $colors = new Color();
+        $colors->name = $this->name;
+        $colors->code = $this->code;
+        $colors->save();
+
+    }
+
+    public function mount()
+    {
+        $this->colors = Color::all();
+    }
+
+
     public function render()
     {
-        return view('admin.livewire.color.index')->extends('admin.layouts.app');
+        $colors = Color::all();
+        return view('admin.livewire.color.index', ['colors' => $colors])->extends('admin.layouts.app');
     }
 }
--- database/migrations/2023_02_10_225404_create_colors_table.php
+++ database/migrations/2023_02_10_225404_create_colors_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('code');
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
