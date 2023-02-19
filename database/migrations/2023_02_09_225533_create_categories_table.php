7e07c4bfe5ec44fcfd935df4a995fed22710a465
LeGion <mehrdad.dadashi@gmail.com>
2023-02-19 13:07:55 +0300

    add nullable to categories table for parent categories

--- database/migrations/2023_02_09_225533_create_categories_table.php
+++ database/migrations/2023_02_09_225533_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('icon');
             $table->timestamps();
         });
